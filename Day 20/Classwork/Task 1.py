bb17220dc2c1ffcc583417ba672044b210877ab6
Nikoloz201516 <nikolabesadze5@gmail.com>
2024-09-08 09:01:38 -0400

    commit

---
+++ Day 20/Classwork/Task 1.py
@@ -0,0 +1,16 @@
+#Create a list of 5 numbers and print the list
+list1=[1,2,3,4,5]
+# Append an item to a list
+list1.append(6)
+# Remove an item from a list
+list1.remove(6)
+#Replace an item in a list
+list1[2]="jjk"
+#print index 3 item in a list
+list1[3]
+#Get the length of a list
+print(len(list1))
+print(list1)
+#Reverse a list
+listn=[1,2,3,4,5]
+print(listn[-1:-2])
